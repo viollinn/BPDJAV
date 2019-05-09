d71efda2674f8e6cbc93a8a5d625058990ecc02b
viollinn <nicolas.viollin@cgi.com>
2019-05-09 15:19:28 +0200

    Ajout initial

--- src/main/java/fr/cgi/nantes/formation/FizzBuzz.java
+++ src/main/java/fr/cgi/nantes/formation/FizzBuzz.java
@@ -9,17 +9,23 @@ public class FizzBuzz
     public static void main( String[] args )
     {
         
+        boolean mod3 = false;
+        boolean mod5 = false;
+
         for (int i = 1; i < 100; i++) {
             
-            if (((i % 3) == 0) & ((i % 5) == 0)){
+            mod3 = ((i % 3) == 0);
+            mod5 = ((i % 5) == 0);
+
+            if (mod3 & mod5){
                 
                 System.out.println("FizzBuzz");
 
-            }else if ((i % 3) == 0){
+            }else if (mod3){
 
                 System.out.println("Fizz");
 
-            }else if ((i % 5) == 0){
+            }else if (mod5){
 
                 System.out.println("Buzz");
 
